62a046a3c09bb574646230a27fd4b92d85e7d562
shree-2023 <132129359+shree-2023@users.noreply.github.com>
2024-07-20 11:55:01 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,65 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Headersection from './Components/Headersection/Headersection'
-import { Title } from './Components/Title/Title'
-import About from './Components/About/About'
-import Contact from './Components/Contact/Contact'
-import Project from './Components/Projects/Project'
+import React, { useState, useEffect } from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Headersection from './Components/Headersection/Headersection';
+import { Title } from './Components/Title/Title';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Project from './Components/Projects/Project';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+const Component = () => {
+  const { height, width } = useWindowDimensions();
+
+  return (
+    <div >
+      <p style={{
+      bottom: 0, 
+      width: '100%', 
+      textAlign: 'center', 
+      backgroundColor: 'lightblue', 
+      fontStyle: 'italic', 
+      padding: '1px 0'  } }>Shreelakshmi Hegde</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-      
     <div>
-      
       <Navbar />
       <Headersection />
-      <div className='container'>
-     
+      <div className="container">
         <About />
-        <Title subTitle='MY PROJECTS' />
-        <Project/>
-        <Title subTitle='CONTACT DETAILS'  />
+        <Title subTitle="MY PROJECTS" />
+        <Project />
+        <Title subTitle="CONTACT DETAILS" />
         <Contact />
-        
       </div>
+      <Component />
     </div>
-   
-  )
+  );
 }
-export default App
+
+export default App;
--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -6,36 +6,46 @@ import program_3 from '../../assets/m6.webp'
 import program_icon_1 from '../../assets/program-icon-1.png'
 import program_icon_2 from '../../assets/program-icon-2.png'
 import program_icon_3 from '../../assets/program-icon-3.png'
+import image from '../../assets/image.png'
 
 const Project= () => {
   return (
     <div className='pgms'>
         <div className='pgm'>
        
-            <img src={program_1} alt="" />
+            <img src={program_1} height={"180dp"} width={"170dp"} alt="" />
             <div className="caption">
            
             <a href=" https://github.com/shree-2023/SOS2" target="_blank" rel="noopener noreferrer">
-                <img src={program_icon_2} alt="SOS app" /></a>
+                <img src={program_icon_2} height={"50dp"} width={"50dp"} alt="SOS app"  /></a>
             <p> SOS app</p>
             </div>
         </div>
         <div className='pgm'>
-            <img src={program_2} alt="" />
+            <img src={program_2}  height={"180dp"} width={"170dp"} alt="" />
             <div className="caption">
             <a href="https://github.com/shree-2023/Voice_controlled_emergency_alert_system" target="_blank" rel="noopener noreferrer">
-                <img src={program_icon_2} alt="Voice controlled Emergency Alert System" />
+                <img src={program_icon_2} height={"50dp"} width={"50dp"} alt="Voice controlled Emergency Alert System" />
             </a>
             <p>Voice controlled Emergency Alert System</p>
             </div>
 
         </div>
         <div className='pgm'>
-            <img src={program_3} alt="" />
+            <img src={program_3}  height={"180dp"} width={"170dp"} alt="" />
             <div className="caption">
             <a href=" https://github.com/shree-2023/Food_waste_management_system" target="_blank" rel="noopener noreferrer">
-                <img src={program_icon_2} alt="Food Waste Management System" /></a>
+                <img src={program_icon_2} height={"50dp"} width={"50dp"} alt="Food Waste Management System" /></a>
             <p> Food Waste Management System</p>
+            </div>
+        </div>
+
+        <div className='pgm'>
+            <img src={image} height={"180dp"} width={"170dp"} alt="" />
+            <div className="caption">
+            <a href=" https://github.com/shree-2023/PersonalisedLearning-System" target="_blank" rel="noopener noreferrer">
+                <img src={program_icon_2}  height={"50dp"} width={"50dp"}alt="Personalized Resource Suggestion System" /></a>
+            <p> Personalized Resource Suggestion System</p>
             </div>
         </div>
     </div>
